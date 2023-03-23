6e3bbefb5548d24da3659cd6583f77cdb4c3f6f3
Abderrahmane Mya <amya@e1r7p15.1337.ma>
2023-03-23 00:19:10 +0000

    working on loging handler

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
+
+
+// next.config.js
+module.exports = {
+  images: {
+    domains: ['assets.nflxext.com', 'image.tmdb.org'],
+  },
+}
